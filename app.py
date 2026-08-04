b53ab3a16a3a402a3cf94f9d3526760277708b78
tcullen-tc <tcullen.tc@gmail.com>
2026-08-04 11:17:10 -0500

    Moved email credentials to environment variables

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ app.secret_key = 'family-memories-secret-key'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'cullenfamily.memories@gmail.com'
-app.config['MAIL_PASSWORD'] = 'dvqtfwzzpvlpsfac'
-app.config['MAIL_DEFAULT_SENDER'] = 'cullenfamily.memories@gmail.com'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', 'cullenfamily.memories@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME', 'cullenfamily.memories@gmail.com')
 
 mail = Mail(app)
 
